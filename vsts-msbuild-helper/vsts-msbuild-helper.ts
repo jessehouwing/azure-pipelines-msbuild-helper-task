03a39fe6934cae288ab6a67b1211fb7751d46159
Jesse Houwing <jesse.houwing@gmail.com>
2016-06-17 16:01:19 +0200

    Implements Code Analysis and Layer Validation helpers

--- vsts-msbuild-helper/vsts-msbuild-helper.ts
+++ vsts-msbuild-helper/vsts-msbuild-helper.ts
@@ -1,3 +1,41 @@
 ﻿///<reference path="./typings/main.d.ts" />
 import tl = require("vsts-task-lib/task");
 
+let msbuildAdditionalArguments: string[];
+const variableName = tl.getInput("variableName", true);
+
+// CODE ANALYSIS
+const runCodeAnalysis = tl.getInput("RunCodeAnalysis", false);
+if (runCodeAnalysis !== "") {
+    msbuildAdditionalArguments.push(`/p:RunCodeAnalysis=${runCodeAnalysis}`);
+}
+
+const codeAnalysisRuleset = tl.getInput("CodeAnalysisRuleset", false);
+if (codeAnalysisRuleset !== "") {
+    if (codeAnalysisRuleset !== "Custom") {
+        msbuildAdditionalArguments.push(`/p:CodeAnalysisRuleset=${codeAnalysisRuleset}`);
+    } else {
+        const customCodeAnalysisRuleset = tl.getPathInput("CustomCodeAnalysisRuleset", true);
+        msbuildAdditionalArguments.push(`/p:CodeAnalysisRuleset=${customCodeAnalysisRuleset}`);
+    }
+}
+
+const codeAnalysisTreatWarningsAsErrors = tl.getBoolInput("CodeAnalysisTreatWarningsAsErrors", false);
+if (codeAnalysisTreatWarningsAsErrors) {
+    msbuildAdditionalArguments.push(`/p:CodeAnalysisTreatWarningsAsErrors=${codeAnalysisTreatWarningsAsErrors}`);
+}
+
+const codeAnalysisAdditionalArguments = tl.getInput("CodeAnalysisAdditionalArguments", false);
+if (codeAnalysisAdditionalArguments) {
+    msbuildAdditionalArguments.push(codeAnalysisAdditionalArguments);
+}
+
+
+
+// LAYER VALIDATION
+const validateArchitecture = tl.getInput("ValidateArchitecture", false);
+if (validateArchitecture !== "") {
+    msbuildAdditionalArguments.push(`/p:ValidateArchitecture=${validateArchitecture}`);
+}
+
+tl.setVariable(variableName, msbuildAdditionalArguments.join(" "));
